58c039191489db8485c8898638f711df89cbb016
suresh170411 <suresh.20fk@gmail.com>
2022-11-10 09:46:34 +0530

    added account service and implementation

--- Backend/DIGI_POCKET/src/main/java/com/project/Services/UserService.java
+++ Backend/DIGI_POCKET/src/main/java/com/project/Services/UserService.java
@@ -7,10 +7,6 @@ import com.project.Model.Customer;
 
 public interface UserService {
 	
-	public Customer registerCustomer(Customer customer);
 	
-	public List<Customer> getAllCostomer() throws CustomerException;
-	
-	public Customer deleteCustomerByMobile(String mobile) throws CustomerException;
 	
 }
