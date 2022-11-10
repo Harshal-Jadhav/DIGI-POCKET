3bd6c2a48fd02828cff3ea95f4acaa8bde04ed09
suresh170411 <suresh.20fk@gmail.com>
2022-11-10 09:01:16 +0530

    UPDATE: customer service class and implementation with some methods added

--- Backend/DIGI_POCKET/src/main/java/com/project/Services/UserService.java
+++ Backend/DIGI_POCKET/src/main/java/com/project/Services/UserService.java
@@ -1,5 +1,16 @@
 package com.project.Services;
 
-public interface UserService {
+import java.util.List;
 
+import com.project.Exceptions.CustomerException;
+import com.project.Model.Customer;
+
+public interface UserService {
+	
+	public Customer registerCustomer(Customer customer);
+	
+	public List<Customer> getAllCostomer() throws CustomerException;
+	
+	public Customer deleteCustomerByMobile(String mobile) throws CustomerException;
+	
 }
